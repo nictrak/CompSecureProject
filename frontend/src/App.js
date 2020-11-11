8faae9372014b7acc987c272a483d15a55664cea
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-11-12 01:13:00 +0700

    Implement a login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Home from './components/home';
+import Login from './components/Login/login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return (<BrowserRouter >
+    <Switch>
+      <Route exact path={'/login'}>
+        <Login />
+      </Route>
+      <Route exact path={'/register'}>
+        <div>Register</div>
+      </Route>
+      <Route exact path={'/home'}>
+        <Home />
+      </Route>
+    </Switch></BrowserRouter>)
 }
 
 export default App;
