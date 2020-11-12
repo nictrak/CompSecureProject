5a55923848c4904f13074d387673a201b4e26434
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-11-12 18:14:26 +0700

    Complete Frontend Authorization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AuthContext from './authContext'
 import Home from './components/home';
-import Login from './components/Login/login'
-import Authentication from './components/authentication'
+import Login from './components/login';
+import Register from './components/register';
+import AuthService from './api/auth.service'
+// import Authentication from './components/authentication'
 
 const App = () => {
+
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    setUser(AuthService.getCurrentUser())
+  }, [])
+
   return (
     <BrowserRouter >
       <Switch>
         <Route exact path={'/'}>
-          <Authentication />
+          {user ? <Home /> : <Redirect to="/login" />}
+        </Route>
+        <Route exact path={'/login'}>
+          <Login />
+        </Route>
+        <Route exact path={'/register'}>
+          <Register />
         </Route>
         <Route render={() => <Redirect to="/" />} />
-        {/* <Route exact path={'/register'}>
-          <div>Register</div>
-        </Route>
-        <Route exact path={'/home'}>
-          <Home />
-        </Route> */}
       </Switch>
     </BrowserRouter>
 
--- frontend/src/components/authentication.js
+++ frontend/src/components/authentication.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import AuthService from '../api/auth.service'
 
 const Authentication = () => {
 
@@ -25,7 +26,44 @@ const Authentication = () => {
         setConfirmedPassword(e.target.value)
     }
 
-    const handleLoginButton = () => {
+    const handleLoginButton = (e) => {
+        e.preventDefault();
+        AuthService.login(username, password);
+        // .then(
+        //     () => {
+        //         window.location.reload();
+        //     },
+        //     error => {
+        //         const resMessage =
+        //             (error.response &&
+        //                 error.response.data &&
+        //                 error.response.data.message) ||
+        //             error.message ||
+        //             error.toString();
+
+        //     }
+        // );
+    }
+
+    const handleRegisterButton = () => {
+
+        AuthService.register(
+            username,
+            password
+        ).then(
+            response => {
+
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+
+            }
+        );
 
     }
 
@@ -45,16 +83,16 @@ const Authentication = () => {
                         <form>
                             <div className="form-group">
                                 <label for="userNameInput">Username</label>
-                                <input type="text" className="form-control" id="userNameInput" onChange={handleUsernameChange} value={username} />
+                                <input type="text" className="form-control" id="userNameInput" onChange={handleUsernameChange} value={username} required />
 
                             </div>
                             <div className="form-group">
                                 <label for="innputPassword">Password</label>
-                                <input type="password" className="form-control" id="innputPassword" onChange={handlePasswordChange} value={password} />
+                                <input type="password" className="form-control" id="innputPassword" onChange={handlePasswordChange} value={password} required />
                             </div>
                             <small className="">Aren't a member yet? <span className='link-to text-primary' onClick={toggleLoginPage}>Register</span></small>
                             <div className="w-100 text-right">
-                                <button type="submit" className="btn btn-primary">Login</button>
+                                <button type="submit" className="btn btn-primary" onClick={handleLoginButton}>Login</button>
                             </div>
 
                         </form>
@@ -71,22 +109,22 @@ const Authentication = () => {
                         <form>
                             <div className="form-group">
                                 <label for="userNameInput">Username</label>
-                                <input type="text" className="form-control" id="userNameInput" onChange={handleUsernameChange} value={username} aria-describedby="usernameHelp" />
-                                <small id="usernameHelp" className="form-text text-muted">Username must contain only alphabets and numbers.</small>
+                                <input type="text" className="form-control" id="userNameInput" onChange={handleUsernameChange} value={username} aria-describedby="usernameHelp" required pattern="^(?=.*[a-zA-Z])(?=\w*[0-9])\w{3,20}$" />
+                                <small id="usernameHelp" className="form-text text-muted">Username must contain 3 to 20 characters, only alphabets or numbers.</small>
                             </div>
                             <div className="form-group">
                                 <label for="innputPassword">Password</label>
-                                <input type="password" className="form-control" id="innputPassword" onChange={handlePasswordChange} value={password} aria-describedby="passwordHelp" />
+                                <input type="password" className="form-control" id="innputPassword" onChange={handlePasswordChange} value={password} aria-describedby="passwordHelp" required required minLength={8} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{8, 20}$" />
                                 <small id="passwordHelp" className="form-text text-muted">Password must contain at least 8 characters.</small>
                             </div>
                             <div className="form-group">
                                 <label for="innputConfirmedPassword">Confirmed Password</label>
-                                <input type="password" className="form-control" id="innputConfirmedPassword" onChange={handleConfirmedPasswordChange} value={confirmedPassword} aria-describedby="confirmedPasswordHelp" />
+                                <input type="password" className="form-control" id="innputConfirmedPassword" onChange={handleConfirmedPasswordChange} value={confirmedPassword} aria-describedby="confirmedPasswordHelp" required minLength={8} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{8, 20}$" />
                                 {password !== '' && confirmedPassword !== '' ? password !== confirmedPassword ? <small id="confirmedPasswordHelp" className="form-text text-danger">Password doesn't match.</small> : <small id="confirmedPasswordHelp" className="form-text text-success">Password matches.</small> : null}
                             </div>
                             <small className="">Already a member! <span className='link-to text-primary' onClick={toggleLoginPage}>Login</span></small>
                             <div className="w-100 text-right">
-                                <button type="submit" className="btn btn-success">Register</button>
+                                <button type="submit" className="btn btn-success" onClick={handleRegisterButton}>Register</button>
                             </div>
 
                         </form>
