527a2dbb5631fbeb92417986308d3b47111339ce
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-11-13 13:42:59 +0700

    Implement Post text box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import AuthContext from './authContext'
 import Home from './components/home';
 import Login from './components/login';
 import Register from './components/register';
-import AuthService from './api/auth.service'
+import PostTextBox from './components/Post/post.textbox';
+import PostDisplay from './components/Post/post.display';
+import AuthService from './api/auth.service';
 // import Authentication from './components/authentication'
 
 const App = () => {
@@ -21,11 +22,17 @@ const App = () => {
         <Route exact path={'/'}>
           {user ? <Home /> : <Redirect to="/login" />}
         </Route>
+        <Route exact path={'/home'}>
+          <PostDisplay />
+        </Route>
         <Route exact path={'/login'}>
           <Login />
         </Route>
         <Route exact path={'/register'}>
           <Register />
+        </Route>
+        <Route exact path={'/post'}>
+          <PostTextBox />
         </Route>
         <Route render={() => <Redirect to="/" />} />
       </Switch>
--- frontend/src/api/user.service.js
+++ frontend/src/api/user.service.js
@@ -3,25 +3,26 @@ import authHeader from './auth-header';
 
 const API_URL = 'http://localhost:8080/api/post/';
 
-const UserService = () => {
+class UserService {
 
-    const getAllPosts = () => {
+    getAllPosts() {
         return axios.get(API_URL + 'all', { headers: authHeader() });
     }
 
-    const getPostByID = (post_id) => {
+    getPostByID(post_id) {
         return axios.get(API_URL + post_id, { headers: authHeader() });
     }
 
-    const createPost = (user_id, content) => {
+    createPost(user_id, content) {
         return axios.post(API_URL + 'create', { user_id: user_id, content: content }, { headers: authHeader() });
+        // console.log('message:', content, 'by:', user_id);
     }
 
-    const deletePost = (post_id) => {
+    deletePost(post_id) {
         return axios.delete(API_URL + 'delete/' + post_id, { headers: authHeader() })
     }
 
-    const updatePost = (post_id, user_id, content) => {
+    updatePost(post_id, user_id, content) {
         return axios.update(API_URL + 'update/' + post_id, { post_id: post_id, user_id: user_id, content: content }, { headers: authHeader() })
     }
 }
