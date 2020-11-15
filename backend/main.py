0924347e9732eb95436d782af2a8da8d0c383d1b
phumnoi1 <phumnoi1@gmail.com>
2020-11-15 18:13:48 +0700

    flask test

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,36 @@
+
 print("Use flask here.")
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest = name))
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+@app.route('/login' , methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success',name = user))
+    else:
+        user = request.args.get('nm')
+        return  redirect(url_for('success',name = user))
+
+
+if __name__ == '__main__' :
+    app.run(debug= True)
