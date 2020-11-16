0e775beb918bf60e176b108243266e1d990e8f76
phumnoi1 <phumnoi1@gmail.com>
2020-11-16 14:40:36 +0700

    get post

--- backend/main.py
+++ backend/main.py
@@ -68,11 +68,32 @@ def post_create():
 
 @app.route('/post/all' , methods = ['GET'])
 def post_all():
-        return mongo.get_all_post()
+        post_list = mongo.get_all_post()
+        for data in post_list:
+            data['_id'] = str(data['_id'])
+        return post_list
 
 @app.route('/post/<post_id>' , methods = ['GET'])
 def post_post_id(post_id):
-        return mongo.get_one_post(post_id)
+        data = mongo.get_one_post(post_id)
+        data['_id'] = str(data['_id'])
+        return data
+
+# post
+@app.route('/post/delete/<post_id>', methods=['DELETE'])
+def post_delete(post_id):
+    payload = {
+        'post_id': post_id
+    }
+    if mongo.post(payload):
+        return Response('{}', status=202, mimetype='application/json')
+    return Response('{}', status=400, mimetype='application/json')
+
+@app.route('/post/update/<post_id>', methods=['UPDATE'])
+def post_delete(post_id):
+    # mongo api
+
+    return status
 
 #comment
 @app.route('/post/comment/add', methods=['POST'])
@@ -80,6 +101,22 @@ def post_delete(post_id):
     # mongo api
 
     return status
+
+@app.route('/post/comment/comment_id', methods=['GET'])
+def post_delete(post_id):
+    # mongo api
+
+    return user_id , content , create_at , post_id
+
+@app.route('/post/comment/delete/comment_id', methods=['DELETE'])
+def post_delete(post_id):
+    # mongo api
+
+    return status
+
+@app.route('/post/comment/add', methods=['UPDATE'])
+def post_delete(post_id):
+    # mongo api
 
 
 if __name__ == '__main__' :
