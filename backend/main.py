e2decbe57d4e4809f4789d2053b6b3d09b46c803
phumnoi1 <phumnoi1@gmail.com>
2020-11-16 14:12:33 +0700

    delete test code

--- backend/main.py
+++ backend/main.py
@@ -12,39 +12,6 @@ mongo = database.MongoDB()
 
 app = Flask(__name__)
 
-#test
-"""
-@app.route('/admin')
-def hello_admin():
-    return 'Hello Admin'
-
-@app.route('/guest/<guest>')
-def hello_guest(guest):
-    return 'Hello %s as Guest' % guest
-
-@app.route('/user/<name>')
-def hello_user(name):
-    if name == 'admin':
-        return redirect(url_for('hello_admin'))
-    else:
-        return redirect(url_for('hello_guest', guest = name))
-
-
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
-
-@app.route('/login' , methods = ['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success',name = user))
-    else:
-        user = request.args.get('nm')
-        return  redirect(url_for('success',name = user))
-"""
-#test end
-
 #finish-----------------------------------------------------------
 
 #unfinish---------------------------------------------------------
