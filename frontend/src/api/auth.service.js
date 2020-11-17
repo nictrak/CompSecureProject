8261cfdcde29eb8fc55d66fae0f62d0e76fb32b6
Weerayut Thinchamlong <weerayut.thc@gmail.com>
2020-11-17 23:19:54 +0700

    Remove proxy

--- frontend/src/api/auth.service.js
+++ frontend/src/api/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/";
+const API_URL = "http://127.0.0.1:5000/api/";
 
 class AuthService {
     login(username, password) {
--- frontend/src/api/user.service.js
+++ frontend/src/api/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 import AuthService from './auth.service'
 
-const API_URL = '/api/post/';
+const API_URL = 'http://127.0.0.1:5000/api/post/';
 
 class UserService {
 
